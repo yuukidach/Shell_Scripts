d253a9f50bdcd444e8ae4963618728f5ea5db25f
yuukidach <chendamailbox@foxmail.com>
2017-03-01 19:28:07 +0800

    增加搜索层数限制

--- srtrename.sh
+++ srtrename.sh
@@ -2,7 +2,7 @@
 
 echo "Before the opration stars, please make sure you have been in the right folder!"
 while true; do
-    read -p "Do you want to continue? y/n  " yn
+    read -p "Do you want to continue? y/n " yn
     case ${yn} in
         [Yy]* ) break;;
         [Nn]* ) exit;;
@@ -19,14 +19,14 @@ read total_video_episode_num
 echo "These files were renamed successfully:"
 for target_episode in `seq -w 1 ${total_video_episode_num}`            # -w使输出的数字同宽
 do
-    target_video=`find -regex ".*[^0-9]?${target_episode}[^0-9]?.*${video_extension_name}"`
+    target_video=`find -maxdepth 1 -regex ".*[^0-9ax_]${target_episode}[^0-9Pp].*${video_extension_name}"`
     target_video=${target_video%${video_extension_name}}
     target_video=${target_video:2}
     echo "${target_video}"
-    find -regex ".*[^0-9\.i]${target_episode}[^0-9\.P].*ass" | while IFS=" " read target_sub
+    find -maxdepth 1 -regex ".*[^0-9aix]${target_episode}[^0-9\.Pp].*ass" | while IFS=" " read target_sub
     do
         echo "${target_sub}"
-        rename "s/.*[^0-9\.i]${target_episode}[^0-9\.P].*\./${target_video}/" "${target_sub}"
+        rename "s/.*[^0-9aix]${target_episode}[^0-9\.Pp].*\./${target_video}/" "${target_sub}"
     done
 done
 
